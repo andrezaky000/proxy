83522fb498aef246c890d763de6c4d16e01ab099
andrezaky000 <az13.5k@yahoo.com>
2025-03-19 20:05:04 -0700

    Update and rename proxy.js.txt to proxy.js

--- proxy.js
+++ proxy.js
@@ -14,6 +14,6 @@ const server = http.createServer((req, res) => {
     req.pipe(proxyReq, { end: true });
 });
 
-server.listen(3000, () => {
-    console.log('Proxy server running on http://localhost:3000');
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Proxy server is running');
 });
